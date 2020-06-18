9c56749e79d40f07e9fcd4a63f8cd2b3dc0c44f1
tianqi <li.tianq@husky.neu.edu>
2020-06-18 16:57:17 -0400

    add

---
+++ src/main/java/com/example/project/models/ReviewId.java
@@ -0,0 +1,16 @@
+package com.example.project.models;
+public class ReviewId {
+    private long userId;
+    private long sectionId;
+    public int hashCode() {
+        return (int)(userId + sectionId);
+    }
+
+    public boolean equals(Object object) {
+        if (object instanceof ReviewId) {
+            ReviewId otherId = (ReviewId) object;
+            return (otherId.userId == this.userId) && (otherId.sectionId == this.sectionId);
+        }
+        return false;
+    }
+}
